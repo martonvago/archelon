1145838a439d3b2a0e6d22834110b657021a269c
martonvago <marci.vago@gmail.com>
2020-12-05 19:54:02 +0000

    AMS-3: add tests for main nav graph; factor out shared nav test logic

--- app/src/androidTest/java/com/martonvago/archelon/navigation/CreateSurveyNavigationTest.kt
+++ app/src/androidTest/java/com/martonvago/archelon/navigation/CreateSurveyNavigationTest.kt
@@ -1,23 +1,21 @@
 package com.martonvago.archelon.navigation
 
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelStore
-import androidx.navigation.Navigation
-import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers
-import com.google.common.truth.Truth.assertThat
 import com.martonvago.archelon.R
-import com.martonvago.archelon.launchFragmentInHiltContainer
 import com.martonvago.archelon.ui.createsurvey.eventsmenu.MorningSurveyEventsMenuFragment
 import com.martonvago.archelon.ui.createsurvey.menu.MorningSurveyMenuFragment
 import com.martonvago.archelon.ui.createsurvey.observersweather.MorningSurveyObserversWeatherFragment
 import com.martonvago.archelon.ui.createsurvey.placetime.MorningSurveyPlaceTimeFragment
 import org.junit.jupiter.api.*
 
-class SurveyNavigationTest {
+class CreateSurveyNavigationTest: NavigationTestBase() {
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll(){
+            setUpNavController(R.navigation.morning_survey_nav_graph)
+        }
+    }
 
     @Nested
     @DisplayName("Test navigation from and back to place/time screen")
@@ -147,37 +145,11 @@ class SurveyNavigationTest {
         assertThatCancelDialogOpens()
     }
 
-    companion object {
-        lateinit var navController: TestNavHostController
+    private fun assertThatCancelDialogOpens() {
+        // when
+        clickElementWithId(R.id.cancelButton)
 
-        @BeforeAll
-        @JvmStatic
-        fun beforeAll() {
-            navController = TestNavHostController(ApplicationProvider.getApplicationContext())
-            navController.setViewModelStore(ViewModelStore())
-            navController.setGraph(R.navigation.morning_survey_nav_graph)
-        }
-
-        fun assertThatCancelDialogOpens() {
-            // when
-            clickElementWithId(R.id.cancelButton)
-
-            // then
-            assertThatCurrentDestinationIs(R.id.cancelSurveyDialogFragment)
-        }
-
-        fun assertThatCurrentDestinationIs(destinationId: Int) {
-            assertThat(navController.currentDestination?.id).isEqualTo(destinationId)
-        }
-
-        fun clickElementWithId(id: Int) {
-            onView(ViewMatchers.withId(id)).perform(ViewActions.click())
-        }
-
-        inline fun <reified T : Fragment> setupScenarioForFragment() {
-            launchFragmentInHiltContainer<T>(themeResId = R.style.AppTheme) {
-                Navigation.setViewNavController(this.view!!, navController)
-            }
-        }
+        // then
+        assertThatCurrentDestinationIs(R.id.cancelSurveyDialogFragment)
     }
 }
---
+++ app/src/androidTest/java/com/martonvago/archelon/navigation/NavigationTestBase.kt
@@ -0,0 +1,43 @@
+package com.martonvago.archelon.navigation
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelStore
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.google.common.truth.Truth.assertThat
+import com.martonvago.archelon.R
+import com.martonvago.archelon.launchFragmentInHiltContainer
+
+abstract class NavigationTestBase {
+
+    // The nav controller is set up in @BeforeAll in the inheriting class, which has to be
+    // enclosed in a companion object. So we have to enclose the shared setup logic
+    // in a companion object here as well.
+    companion object {
+        lateinit var navController: TestNavHostController
+
+        fun setUpNavController(navGraphId: Int) {
+            navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+            navController.setViewModelStore(ViewModelStore())
+            navController.setGraph(navGraphId)
+        }
+    }
+
+    fun assertThatCurrentDestinationIs(destinationId: Int) {
+        assertThat(navController.currentDestination?.id).isEqualTo(destinationId)
+    }
+
+    fun clickElementWithId(id: Int) {
+        onView(ViewMatchers.withId(id)).perform(ViewActions.click())
+    }
+
+    inline fun <reified T : Fragment> setupScenarioForFragment() {
+        launchFragmentInHiltContainer<T>(themeResId = R.style.AppTheme) {
+            Navigation.setViewNavController(this.view!!, navController)
+        }
+    }
+}
