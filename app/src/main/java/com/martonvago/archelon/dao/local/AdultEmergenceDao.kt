90ade28b978106de1a3b209e814cea7f5c7ba705
martonvago <marci.vago@gmail.com>
2020-12-21 14:20:52 +0000

    AMS-9: add db tests

--- app/src/main/java/com/martonvago/archelon/dao/local/AdultEmergenceDao.kt
+++ app/src/main/java/com/martonvago/archelon/dao/local/AdultEmergenceDao.kt
@@ -2,10 +2,11 @@ package com.martonvago.archelon.dao.local
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.martonvago.archelon.entity.AdultEmergence
 
 @Dao
 interface AdultEmergenceDao {
-    @Insert
-    suspend fun save(adultEmergence: AdultEmergence)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(adultEmergence: AdultEmergence): Long
 }
--- app/src/main/java/com/martonvago/archelon/dao/local/HatchingDao.kt
+++ app/src/main/java/com/martonvago/archelon/dao/local/HatchingDao.kt
@@ -2,10 +2,11 @@ package com.martonvago.archelon.dao.local
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.martonvago.archelon.entity.Hatching
 
 @Dao
 interface HatchingDao {
-    @Insert
-    suspend fun save(hatching: Hatching)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(hatching: Hatching): Long
 }
